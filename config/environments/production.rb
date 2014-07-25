0307b917190d52c1285cc98dfe24c53c0522ebca
Jonah Sinick <jsinick@gmail.com>
2014-07-25 10:22:18 -0700

    added assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ SOC::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.assets.compile = true
+
+  config.serve_static_assets = true
+  
 end
