320ebd0157d517b57f921ea88943115b78e35bb6
Jonah Sinick <jsinick@gmail.com>
2014-07-25 10:27:23 -0700

    fixing comment table issue

--- db/migrate/20140724224635_create_comments.rb
+++ db/migrate/20140724224635_create_comments.rb
@@ -1,15 +1,15 @@
-class CreateComments < ActiveRecord::Migration
-  def change
-    drop_table :comments
-    
-    create_table :comments do |t|
-      t.text :body, null: false
-      t.integer :author_id, null: false
-      t.string :author_name, null: false
-      t.integer :commentable_id, null: false      
-      t.string :commentable_type, null: false
-      t.timestamps
-    end
-    add_index :comments, :author_id
-  end
-end
+# class CreateComments < ActiveRecord::Migration
+#   def change
+#     drop_table :comments
+#
+#     create_table :comments do |t|
+#       t.text :body, null: false
+#       t.integer :author_id, null: false
+#       t.string :author_name, null: false
+#       t.integer :commentable_id, null: false
+#       t.string :commentable_type, null: false
+#       t.timestamps
+#     end
+#     add_index :comments, :author_id
+#   end
+# end
